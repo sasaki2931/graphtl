beb1809de626593b1e79653e83377d3a58c99e08
syunsuke sasaki <sasaki2931@gmail.com>
2023-01-31 14:36:22 +0900

    graphpl課題完了

---
+++ app/graphql/types/contact_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Types
+  class ContactType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String
+    field :email, String
+    field :content, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
